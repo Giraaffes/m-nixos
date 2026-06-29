73b527a30ce7be702fec92861d043c4e9cb4374b
Marcus Færk Henriksen <marcusfaerk@gmail.com>
2026-06-29 11:27:33 +0200

    Added java setup

--- modules/common/home/home.nix
+++ modules/common/home/home.nix
@@ -37,8 +37,7 @@
     firefox slack kdePackages.filelight
 
     # Development
-    vscode nodejs pnpm dbeaver-bin postgresql gitkraken
-    pkgsUnstable.claude-code
+    vscode nodejs pnpm yarn dbeaver-bin postgresql gitkraken
     prisma-engines_7 openssl # For Prisma
     stripe-cli
     
